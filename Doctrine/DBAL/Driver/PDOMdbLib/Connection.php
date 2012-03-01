514f24a9e2f85419d49f634221f405f9ec071b3a
Claudio Alarcon Reyes <clalarco@gmail.com>
2012-03-01 16:47:22 -0300

    Adding files

---
+++ Doctrine/DBAL/Driver/PDOMdbLib/Connection.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Haycia\PDOAccessBundle\Doctrine\DBAL\Driver\PDOMdbLib;
+
+/**
+ * PDODblib Connection implementation.
+ *
+ * @since 2.0
+ */
+class Connection extends \Doctrine\DBAL\Driver\PDOConnection implements \Doctrine\DBAL\Driver\Connection {
+
+	protected $_pdoTransactionsSupport = null;
+	protected $_pdoLastInsertIdSupport = null;
+	/**
+	 * @override
+	 */
+	public function quote($value, $type = \PDO::PARAM_STR) {
+		$val = parent::quote($value, $type);
+
+		// Fix for a driver version terminating all values with null byte
+		if (strpos($val, "\0") !== false) {
+			$val = substr($val, 0, -1);
+		}
+
+		return $val;
+	}
+
+	/**
+	 * @return bool PDO_DBlib transaction support
+	 */
+	private function _pdoTransactionsSupported() {
+		if (!is_null($this->_pdoTransactionsSupport)) {
+			return $this->_pdoTransactionsSupport;
+		}
+
+		$supported = false;
+		try {
+			$supported = true;
+			parent::beginTransaction();
+		} catch (\PDOException $e) {
+			$supported = false;
+		}
+		if ($supported) {
+			parent::commit();
+		}
+
+		return $this->_pdoTransactionsSupport = $supported;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rollback() {
+		if ($this->_pdoTransactionsSupported() === true) {
+			parent::rollback();
+		} else {
+			$this->exec('ROLLBACK TRANSACTION');
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function commit() {
+		if ($this->_pdoTransactionsSupported() === true) {
+			parent::commit();
+		} else {
+			$this->exec('COMMIT TRANSACTION');
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function beginTransaction() {
+		if ($this->_pdoTransactionsSupported() === true) {
+			parent::beginTransaction();
+		} else {
+			$this->exec('BEGIN TRANSACTION');
+		}
+	}
+
+	/**
+	 * @return bool PDO_DBlib::lastInsertId support
+	 */
+	private function _pdoLastInsertId() {
+		if (!is_null($this->_pdoLastInsertIdSupport)) {
+			return $this->_pdoLastInsertIdSupport;
+		}
+
+		$supported = false;
+		try {
+			$supported = true;
+			parent::lastInsertId();
+		} catch (\PDOException $e) {
+			$supported = false;
+		}
+
+		return $this->_pdoLastInsertIdSupport = $supported;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function lastInsertId($name = null) {
+		$id = null;
+		if ($this->_pdoLastInsertId() === true) {
+			$id = parent::lastInsertId();
+		} else {
+			$stmt = $this->query('SELECT SCOPE_IDENTITY()');
+			$id = $stmt->fetchColumn();
+			$stmt->closeCursor();
+		}
+
+		return $id;
+	}
+
+}
---
+++ Doctrine/DBAL/Driver/PDOMdbLib/Driver.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ *  $Id$
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.doctrine-project.org>.
+ */
+
+namespace Haycia\PDOAccessBundle\Doctrine\DBAL\Driver\PDOMdbLib;
+
+/**
+ * The PDO-based Dblib driver.
+ *
+ * @since 2.0
+ */
+class Driver implements \Doctrine\DBAL\Driver {
+	public function connect(array $params, $username = null, $password = null, array $driverOptions = array()) {
+		return new Connection(
+			$this->_constructPdoDsn($params),
+			$username,
+			$password,
+			$driverOptions
+		);
+	}
+
+	/**
+	 * Constructs the Dblib PDO DSN.
+	 *
+	 * @return string  The DSN.
+	 */
+	private function _constructPdoDsn(array $params) {
+
+        $dsn = 'odbc:';
+        
+        if (isset($params['host'])) {
+            $dsn .= "Driver=" . $params['host'];
+		}
+        
+		return $dsn;
+	}
+
+	public function getDatabasePlatform() {
+		return new \Doctrine\DBAL\Platforms\MsSqlPlatform();
+		//return new \Doctrine\DBAL\Platforms\SQLServer2005Platform();
+	}
+
+	public function getSchemaManager(\Doctrine\DBAL\Connection $conn) {
+		return new \Doctrine\DBAL\Schema\MsSqlSchemaManager($conn);
+		//return new \Doctrine\DBAL\Schema\SQLServerSchemaManager($conn);
+	}
+
+	public function getName() {
+		return 'pdo_mdblib';
+	}
+
+	public function getDatabase(\Doctrine\DBAL\Connection $conn) {
+		$params = $conn->getParams();
+		return $params['dbname'];
+	}
+}
